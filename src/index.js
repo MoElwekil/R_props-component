1c597dcfa65e5f8c7dbc05dd835bd4996dd39cbf
Mo Elwekil <Mo@descod.net>
2021-06-02 16:45:43 +0100

    Basic 101 Rectjs props project completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} time="Today At 4:00PM" image={faker.image.avatar()} imageAlt={faker.name.firstName()} comment="hello" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} time="Yesterday At 3:00AM" image={faker.image.avatar()} imageAlt={faker.name.firstName()} comment="hello" />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author={faker.name.firstName()} time="Last Friday At 12:00PM" image={faker.image.avatar()} imageAlt={faker.name.firstName()} comment="hello" />
+            </ApprovalCard>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
